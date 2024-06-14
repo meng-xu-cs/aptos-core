88ee69c459b935a367dc9919765de30c096802c0
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-06-14 13:01:17 -0700

    [move][audit] package-level functionalities

--- third_party/move/tools/move-audit/src/lib.rs
+++ third_party/move/tools/move-audit/src/lib.rs
@@ -1,23 +1,126 @@
+mod common;
+mod config;
 mod deps;
-mod util;
+mod package;
+mod testnet;
 
-use crate::deps::{PkgManifest, PkgNamedAddr};
-use anyhow::Result;
-use clap::Subcommand;
-use log::{debug, LevelFilter};
-use std::{collections::BTreeMap, path::Path};
-use walkdir::WalkDir;
+use crate::{
+    common::{Project, Workspace},
+    deps::PkgManifest,
+    testnet::ProfileConfig,
+};
+use anyhow::{anyhow, bail, Result};
+use clap::{Parser, Subcommand};
+use log::{debug, info, LevelFilter};
+use regex::Regex;
+use std::path::PathBuf;
 
 /// Commands for auditing
 #[derive(Subcommand)]
 pub enum AuditCommand {
+    /// Initialize the workspace
+    Init {
+        /// Clean-up previous states
+        #[clap(long)]
+        force: bool,
+    },
+
+    /// List collected packages in the project
     List,
+
+    /// Run unit tests in the packages
+    UnitTest {
+        /// Filter on package level
+        #[clap(flatten)]
+        pkg_filter: FilterPackage,
+
+        /// Compilation only (skip unit tests)
+        #[clap(long)]
+        compile_only: bool,
+    },
 }
 
-/// A Move audit project composed by a list of packages to audit
-pub struct Project {
-    pub pkgs: BTreeMap<String, (PkgManifest, bool)>,
-    pub named_addresses: BTreeMap<String, PkgNamedAddr>,
+/// Package-level filter
+#[derive(Parser)]
+pub struct FilterPackage {
+    /// Include dependencies
+    #[clap(long)]
+    include_deps: bool,
+
+    /// Allow-list
+    #[clap(long)]
+    include_pkg: Option<Vec<String>>,
+
+    /// Deny-list
+    #[clap(long)]
+    exclude_pkg: Option<Vec<String>>,
+}
+
+impl FilterPackage {
+    fn apply<'a>(&self, project: &'a Project) -> Result<Vec<&'a PkgManifest>> {
+        let include_regex = match self.include_pkg.as_ref() {
+            None => None,
+            Some(patterns) => Some(
+                patterns
+                    .iter()
+                    .map(|p| Regex::new(&format!("^{}$", p)).map_err(|e| anyhow!(e)))
+                    .collect::<Result<Vec<_>>>()?,
+            ),
+        };
+        let exclude_regex = match self.exclude_pkg.as_ref() {
+            None => None,
+            Some(patterns) => Some(
+                patterns
+                    .iter()
+                    .map(|p| Regex::new(&format!("^{}$", p)).map_err(|e| anyhow!(e)))
+                    .collect::<Result<Vec<_>>>()?,
+            ),
+        };
+
+        // filtering logic: include first then exclude
+        let mut filtered = vec![];
+        for (name, (manifest, is_primary)) in project.pkgs.iter() {
+            if !is_primary && !self.include_deps {
+                continue;
+            }
+            match include_regex.as_ref() {
+                None => (),
+                Some(regexes) => {
+                    if regexes.iter().all(|r| !r.is_match(name)) {
+                        continue;
+                    }
+                },
+            }
+            match exclude_regex.as_ref() {
+                None => (),
+                Some(regexes) => {
+                    if regexes.iter().any(|r| r.is_match(name)) {
+                        continue;
+                    }
+                },
+            }
+            filtered.push(manifest);
+        }
+
+        Ok(filtered)
+    }
+}
+
+fn cmd_init(project: Project, force: bool) -> Result<()> {
+    // prepare the workspace
+    let wks = Workspace::init(&project, force)?;
+
+    // launch the local testnet
+    let cmd = testnet::launch_local_testnet(&project.root, &wks, true)?;
+
+    // populate the profiles
+    let result = testnet::init_local_testnet_profiles(&project);
+
+    // shutdown the local testnet
+    cmd.interrupt()?;
+
+    // return the profile initialization result
+    result
 }
 
 fn cmd_list(project: Project) {
@@ -31,11 +134,29 @@ fn cmd_list(project: Project) {
     }
 }
 
+fn cmd_unit_test(project: Project, filter: FilterPackage, compile_only: bool) -> Result<()> {
+    // collect named addresses
+    let wks = Workspace::load(&project)?;
+    if !wks.config.is_file() {
+        bail!("unable to find config.yaml in workspace, run init command to initialize first");
+    }
+    let config = ProfileConfig::load(&wks)?;
+
+    // run over each package
+    for pkg in filter.apply(&project)? {
+        info!("running unit tests for package {}", pkg.name);
+        package::exec_unit_test(pkg, &config, compile_only)?;
+    }
+
+    // done
+    Ok(())
+}
+
 /// Entrypoint on multi-package auditing
 pub fn run_on(
-    path: &Path,
+    path: PathBuf,
     skip_deps_update: bool,
-    verbosity: u8,
+    verbose: u8,
     command: AuditCommand,
 ) -> Result<()> {
     // initialize logging
@@ -43,7 +164,7 @@ pub fn run_on(
         .format_timestamp(None)
         .format_target(false)
         .format_module_path(false)
-        .filter_level(match verbosity {
+        .filter_level(match verbose {
             0 => LevelFilter::Warn,
             1 => LevelFilter::Info,
             2 => LevelFilter::Debug,
@@ -52,22 +173,23 @@ pub fn run_on(
         .init();
     debug!("auditing project at path: {}", path.to_string_lossy());
 
-    // find Move packages with the project directory
-    let mut pkgs = vec![];
-    for entry in WalkDir::new(path) {
-        let entry = entry?;
-        let entry_path = entry.path();
-        if entry_path.file_name().expect("filename") == "Move.toml" {
-            pkgs.push(entry_path.parent().expect("parent").to_path_buf());
-        }
-    }
-
     // resolve the project
-    let project = deps::resolve(pkgs, skip_deps_update)?;
+    let project = deps::resolve(path, skip_deps_update)?;
 
     // execute the command
     match command {
-        AuditCommand::List => cmd_list(project),
+        AuditCommand::Init { force } => {
+            cmd_init(project, force)?;
+        },
+        AuditCommand::List => {
+            cmd_list(project);
+        },
+        AuditCommand::UnitTest {
+            pkg_filter,
+            compile_only,
+        } => {
+            cmd_unit_test(project, pkg_filter, compile_only)?;
+        },
     }
 
     // done
