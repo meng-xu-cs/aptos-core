014e70ae3a1f1519ffa668a296b3faae3788eec2
Meng Xu <meng.xu.cs@uwaterloo.ca>
2025-01-17 10:52:05 -0500

    [fuzz] datatype analysis

--- third_party/move/tools/move-audit/src/fuzz/mod.rs
+++ third_party/move/tools/move-audit/src/fuzz/mod.rs
@@ -1,3 +1,22 @@
-pub mod executor;
+use crate::common::PkgDefinition;
+use anyhow::Result;
 
 mod account;
+mod executor;
+mod ident;
+mod model;
+mod typing;
+
+/// Entrypoint for the fuzzer
+pub fn run_on(pkg_defs: Vec<PkgDefinition>) -> Result<()> {
+    // initialize the tracing executor
+    let mut executor = executor::TracingExecutor::new();
+    executor.provision(&pkg_defs)?;
+
+    // build a model on the packages
+    let mut model = model::Model::new();
+    model.provision(&pkg_defs);
+
+    // done
+    Ok(())
+}
