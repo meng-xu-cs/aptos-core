acf2a9b728d03c5d4f0997a14e5e4f36fa191fdb
Meng Xu <meng.xu.cs@uwaterloo.ca>
2025-01-18 00:12:43 -0500

    [fuzz] on driver generator

---
+++ third_party/move/tools/move-audit/src/fuzz/driver.rs
@@ -0,0 +1,29 @@
+use crate::fuzz::{
+    entrypoint::{FunctionDecl, FunctionRegistry},
+    typing::DatatypeRegistry,
+};
+
+/// A driver generator that also caches information during driver generation
+pub struct DriverGenerator<'a> {
+    datatype_registry: &'a DatatypeRegistry,
+    function_registry: &'a FunctionRegistry,
+}
+
+impl<'a> DriverGenerator<'a> {
+    /// Create a new generator with necessary information
+    pub fn new(
+        datatype_registry: &'a DatatypeRegistry,
+        function_registry: &'a FunctionRegistry,
+    ) -> Self {
+        Self {
+            datatype_registry,
+            function_registry,
+        }
+    }
+
+    /// Generate drivers (which could be zero to multiple) for an entrypoint
+    pub fn generate(&mut self, decl: &FunctionDecl) {
+        log::debug!("processing {}", decl.ident());
+        // TODO
+    }
+}
