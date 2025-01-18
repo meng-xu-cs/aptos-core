35a977839a35c8dee4a3954f53d61f23f10ca9ad
Meng Xu <meng.xu.cs@uwaterloo.ca>
2025-01-17 21:19:30 -0500

    [fuzz] function signature analysis

--- third_party/move/tools/move-audit/src/fuzz/model.rs
+++ third_party/move/tools/move-audit/src/fuzz/model.rs
@@ -1,10 +1,14 @@
-use crate::{common::PkgDefinition, fuzz::typing::DatatypeRegistry};
+use crate::{
+    common::PkgDefinition,
+    fuzz::{entrypoint::FunctionRegistry, typing::DatatypeRegistry},
+};
 use move_compiler::compiled_unit::CompiledUnit;
 use move_package::compilation::compiled_package::CompiledUnitWithSource;
 
 /// A database that holds information we can statically get from the packages
 pub struct Model {
     datatype_registry: DatatypeRegistry,
+    function_registry: FunctionRegistry,
 }
 
 impl Model {
@@ -12,6 +16,7 @@ impl Model {
     pub fn new() -> Self {
         Self {
             datatype_registry: DatatypeRegistry::new(),
+            function_registry: FunctionRegistry::new(),
         }
     }
 
@@ -31,6 +36,24 @@ impl Model {
 
                 // go over all datatypes defined
                 self.datatype_registry.analyze(module);
+            }
+        }
+
+        // initialize the function registry
+        for pkg in pkgs {
+            for CompiledUnitWithSource {
+                unit,
+                source_path: _,
+            } in &pkg.as_built_package().package.root_compiled_units
+            {
+                let module = match unit {
+                    CompiledUnit::Script(_) => continue,
+                    CompiledUnit::Module(m) => &m.module,
+                };
+
+                // go over all datatypes defined
+                self.function_registry
+                    .analyze(&self.datatype_registry, module);
             }
         }
     }
