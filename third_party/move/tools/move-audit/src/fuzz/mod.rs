03a7c1bdfbb8d5b566bfd59a0f9cf457402bb23c
Meng Xu <meng.xu.cs@uwaterloo.ca>
2025-01-24 16:27:46 -0500

    [fuzz] driver generation continued

--- third_party/move/tools/move-audit/src/fuzz/mod.rs
+++ third_party/move/tools/move-audit/src/fuzz/mod.rs
@@ -10,14 +10,14 @@ mod model;
 mod typing;
 
 /// Entrypoint for the fuzzer
-pub fn run_on(pkg_defs: Vec<PkgDefinition>) -> Result<()> {
+pub fn run_on(pkg_defs: Vec<PkgDefinition>, type_recursion_depth: usize) -> Result<()> {
     // initialize the tracing executor
     let mut executor = executor::TracingExecutor::new();
     executor.provision(&pkg_defs)?;
 
     // build a model on the packages
     let mut model = model::Model::new();
-    model.provision(&pkg_defs);
+    model.provision(&pkg_defs, type_recursion_depth);
 
     // done
     Ok(())
