0090bdde0013f2a7c088170e2f2ea6f0d4cd2916
Meng Xu <meng.xu.cs@uwaterloo.ca>
2025-02-01 12:31:50 -0400

    [fuzz] split canvas to a new module

--- third_party/move/tools/move-audit/src/fuzz/model.rs
+++ third_party/move/tools/move-audit/src/fuzz/model.rs
@@ -1,10 +1,16 @@
 use crate::{
     common::PkgDefinition,
-    fuzz::{driver::DriverGenerator, entrypoint::FunctionRegistry, typing::DatatypeRegistry},
+    fuzz::{
+        canvas::{TypeClosureBase, TypeClosureItem},
+        driver::DriverGenerator,
+        entrypoint::{FunctionDecl, FunctionInst, FunctionRegistry},
+        typing::DatatypeRegistry,
+    },
 };
+use itertools::Itertools;
 use move_compiler::compiled_unit::CompiledUnit;
 use move_package::compilation::compiled_package::CompiledUnitWithSource;
-use std::collections::BTreeMap;
+use std::collections::BTreeSet;
 
 /// A database that holds information we can statically get from the packages
 pub struct Model {}
@@ -55,41 +61,105 @@ impl Model {
             }
         }
 
-        // initialize the generator
-        let mut generator =
-            DriverGenerator::new(&datatype_registry, &function_registry, type_recursion_depth);
-
-        // start with collecting instantiations
-        let mut primary_function_instantiations = BTreeMap::new();
+        // collect instantiations of entrypoint functions
+        let mut primary_entrypoint_instantiations = BTreeSet::new();
         for decl in function_registry.iter_decls() {
             if !decl.is_primary {
                 continue;
             }
-            let insts = generator.collect_function_insts(decl);
-            let existing = primary_function_instantiations.insert(decl.clone(), insts);
-            assert!(existing.is_none());
+
+            for inst in
+                collect_function_instantiations(&datatype_registry, decl, type_recursion_depth)
+            {
+                self.analyze_function_instantiation(&datatype_registry, decl, &inst);
+                let inserted = primary_entrypoint_instantiations.insert(inst);
+                assert!(inserted);
+            }
         }
 
-        // fixedpoint iteration to deduce the scripts we can generate
-        loop {
-            let mut num_added = 0;
-            for (decl, insts) in primary_function_instantiations.iter() {
-                for inst in insts {
-                    if generator.has_function_inst(inst) {
-                        continue;
-                    }
-                    if generator.analyze_function_inst(decl, inst) {
-                        num_added += 1;
-                    }
-                }
-            }
+        // initialize the generator
+        let mut generator = DriverGenerator::new(&datatype_registry, &function_registry);
+    }
 
-            if num_added == 0 {
-                break;
-            }
+    fn analyze_function_instantiation(
+        &mut self,
+        datatype_registry: &DatatypeRegistry,
+        decl: &FunctionDecl,
+        inst: &FunctionInst,
+    ) {
+        debug_assert_eq!(decl.ident, inst.ident);
 
-            // otherwise, continue with a new round of iteration
-            log::debug!("number of primary function analyzed: {num_added}",);
+        // instantiate parameter and return types
+        let params: Vec<_> = decl
+            .parameters
+            .iter()
+            .map(|t| {
+                TypeClosureItem::from(datatype_registry.instantiate_type_ref(t, &inst.type_args))
+            })
+            .collect();
+        let ret_ty: Vec<_> = decl
+            .return_sig
+            .iter()
+            .map(|t| {
+                TypeClosureItem::from(datatype_registry.instantiate_type_ref(t, &inst.type_args))
+            })
+            .collect();
+
+        // prepare canvas for the arguments
+        for item in &params {
+            match item {
+                TypeClosureItem::Base(TypeClosureBase::Simple(_))
+                | TypeClosureItem::ImmRef(TypeClosureBase::Simple(_))
+                | TypeClosureItem::MutRef(TypeClosureBase::Simple(_)) => (),
+                TypeClosureItem::Base(TypeClosureBase::Complex(t)) => {
+                    // TODO: if type is copy-able, we can also search for refs
+                    // log::info!("function {inst} requires {}", TypeBase::from(t.clone()));
+                },
+                TypeClosureItem::ImmRef(TypeClosureBase::Complex(t)) => {
+                    // TODO
+                    // log::info!("function {inst} requires &{}", TypeBase::from(t.clone()));
+                },
+                TypeClosureItem::MutRef(TypeClosureBase::Complex(t)) => {
+                    // TODO
+                    // log::info!("function {inst} requires &mut {}",TypeBase::from(t.clone()));
+                },
+            }
         }
     }
+}
+
+/// Utility: collect possible function instantiations
+fn collect_function_instantiations(
+    datatype_registry: &DatatypeRegistry,
+    decl: &FunctionDecl,
+    type_recursion_depth: usize,
+) -> Vec<FunctionInst> {
+    let mut result = vec![];
+
+    // shortcut when this function is not a generic function
+    if decl.generics.is_empty() {
+        result.push(FunctionInst {
+            ident: decl.ident.clone(),
+            type_args: vec![],
+        });
+        return result;
+    }
+
+    // instantiate each of the required type argument
+    let mut ty_args_combo = vec![];
+    for constraint in &decl.generics {
+        let ty_args =
+            datatype_registry.type_bases_by_ability_constraint(*constraint, type_recursion_depth);
+        ty_args_combo.push(ty_args);
+    }
+
+    for inst in ty_args_combo.iter().multi_cartesian_product() {
+        result.push(FunctionInst {
+            ident: decl.ident.clone(),
+            type_args: inst.into_iter().cloned().collect(),
+        });
+    }
+
+    // done with the collection
+    result
 }
